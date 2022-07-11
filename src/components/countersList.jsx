0981fb66396b111cf1cb6304b740899cfae609bc
Tai_SaWFly <vawer24@gmail.com>
2022-07-11 18:03:36 +0500

    app-react

---
+++ src/components/countersList.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import Counter from "./counter";
+
+const CountersList = () => {
+  const initialState = [
+    { id: 0, value: 0, name: "Ненужная вещь" },
+    { id: 1, value: 4, name: "Ложка" },
+    { id: 2, value: 0, name: "Вилка" },
+    { id: 3, value: 0, name: "Тарелка" },
+    { id: 4, value: 0, name: "Набор минималиста" },
+  ];
+
+  const [counters, setCounters] = useState(initialState);
+
+  const handleIncrement = (id) => {
+    const counterIncrement = counters.filter((c) =>
+      c.id === id ? (c.value = c.value + 1) : c
+    );
+    setCounters(counterIncrement);
+  };
+
+  const handleDecrment = (id) => {
+    const counterDecrment = counters.filter((c) =>
+      c.id === id ? (c.value = c.value - 1) : c
+    );
+    setCounters(counterDecrment);
+  };
+
+  const handleDelete = (id) => {
+    const newCounters = counters.filter((c) => c.id !== id);
+    setCounters(newCounters);
+  };
+
+  const handleReset = () => {
+    setCounters(initialState);
+  };
+
+  return (
+    <>
+      {counters.map((count) => (
+        <Counter
+          key={count.id}
+          onDelete={handleDelete}
+          onIncrement={handleIncrement}
+          onDecrment={handleDecrment}
+          {...count}
+        />
+      ))}
+      <button className="btn btn-primary btn-sm m-2" onClick={handleReset}>
+        Сброс
+      </button>
+    </>
+  );
+};
+
+export default CountersList;
